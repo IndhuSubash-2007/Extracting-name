95c6925d8d6acf056086beeb0e694eeef655c091
IndhuSubash-2007 <indhusubash2028@cs.sjcetpalai.ac.in>
2024-10-08 15:04:50 +0530

    Add files via upload

---
+++ string extracting first name.py
@@ -0,0 +1,14 @@
+'''
+Author: Indhu Subash
+Date:08-10-2024
+Version:1.1
+'''
+
+
+first_name=input("Enter your first name:")
+last_name=input("Enter your last name:")
+full_name=first_name+" "+last_name
+print(full_name)
+length=len(first_name)
+extracted_first_name=full_name[:length]
+print(extracted_first_name)
